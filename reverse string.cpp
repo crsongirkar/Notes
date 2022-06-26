2525b6830dd9e422acdb2ddbd543c83f13344ec7
Chinmay Songirkar <73063069+crsongirkar@users.noreply.github.com>
2022-06-26 17:49:52 +0530

    Create reverse string.cpp

---
+++ reverse string.cpp
@@ -0,0 +1,15 @@
+// using reverse function 
+// tc is O(1)
+
+#include<bits/stdc++.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+  string chinmay="chinmay";
+  reverse (chinmay.begin(),chinmay.end());
+  cout<<chinmay<<endl;
+  
+}
+
+//
