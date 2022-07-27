743bbc12a54830d055c9be26bb4f4e100405106d
Chinmay Songirkar <73063069+crsongirkar@users.noreply.github.com>
2022-07-27 22:05:46 +0530

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,63 @@
+array basic ; 
+accessing a postion index; 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n[5]={7, 5, 6, 12, 35};
+
+  // cin>>m;
+//   for(int i=0;i<5;i++)
+     cout<<n[1]<<endl;
+   
+ 
+    
+     
+}
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  float n[5]={1.0,2.0,3.0,4.8,5.5};
+//   cin>>n;
+
+  // cin>>m;
+// for(int i=0;i<n;i++)
+        cout<<n<<endl;
+     cout<<n[0]<<endl;
+   
+ 
+    
+     
+}
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n[5]={7, 5, 6, 12, 35};
+ char ch[5]={'a','b','c','d','e'};
+
+ cin>>m;
+ for(int i=0;i<5;i++)
+    cout<<ch<<endl;
+  cout<<ch[2]<<endl;
+   
+ 
+    
+     
+} 
+
+#include<bits/stdc++.h>
+using namespace std;
+#define n 5;
+
+int main()
+{
+    int sum=10;
+     sum+=n;
+    cout<<sum;
+}
