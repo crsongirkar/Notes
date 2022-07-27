315a34fcfbead6b8d4102e934dbe24c83e47d33e
Chinmay Songirkar <73063069+crsongirkar@users.noreply.github.com>
2022-07-27 14:24:36 +0530

    Create prime num.cpp

---
+++ prime num.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int a;
+  cin>>a;
+  
+  for(int i=0;i<a;i++)
+  {
+    //cout<<i;
+    if(i%2==0)
+  {
+    cout<<i<<" number is prime "<<endl;
+  }
+  else{
+    cout<<i<<" number is odd "<<endl;
+  }
+  
+  }
+  
+}
