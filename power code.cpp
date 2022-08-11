a7d685ebbd27f28ef2b71bb0afadda98c1006d7b
Chinmay Songirkar <73063069+crsongirkar@users.noreply.github.com>
2022-08-11 15:26:10 +0530

    Create power code.cpp

---
+++ power code.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main() {
+    //Write your code here
+int x,n;
+    cin>>x>>n;
+    int ans=0;
+    ans=pow(x,n);
+    cout<<ans<<endl;
+   // ans=x**n;
+    //cout<<x**n<<endl;
+    return 0;
+}
+
+//output:-
+// 2^5 =32
