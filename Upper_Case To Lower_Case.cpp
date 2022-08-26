e95b11a1cd732f629f624c5907cac789205b9608
Chinmay Songirkar <73063069+crsongirkar@users.noreply.github.com>
2022-08-26 16:20:38 +0530

    Create Upper_Case To Lower_Case.cpp

---
+++ Upper_Case To Lower_Case.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a="CHINMAY";
+    
+    for(int i=0;i<a.length();i++)
+    putchar(tolower(a[i]));
+    //cout<<a;
+    
+    return 0;
+}
+//use built in function
