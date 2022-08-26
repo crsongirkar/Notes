f8d6a83b45423bdd9e61dbbe18dbbaec3d02245a
Chinmay Songirkar <73063069+crsongirkar@users.noreply.github.com>
2022-08-26 16:21:11 +0530

    Create Lower_case to Upper_case .cpp

---
+++ Lower_case to Upper_case .cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string a="chinmay";
+    
+    for(int i=0;i<a.length();i++)
+    putchar(toupper(a[i]));
+    //cout<<a;
+    
+    return 0;
+}
