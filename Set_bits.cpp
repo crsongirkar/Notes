57f0627c3f35c2d16fb2f39d216b8d154ba0b8bb
Chinmay Songirkar <73063069+crsongirkar@users.noreply.github.com>
2022-08-27 19:40:28 +0530

    Sets_bits

---
+++ Set_bits.cpp
@@ -0,0 +1,29 @@
+
+
+
+#include <iostream>
+
+using namespace std;
+
+    //Write your countBits function here
+
+int countBits(int n)
+{
+    int count=0;
+    while(n!=0)
+    {
+        if(n&1==1)
+        {
+            count+=1;
+            
+        }
+        n=n>>1;
+    }
+    return count;
+}
+int main() {
+    int n;
+    cin >> n;
+    cout<<countBits(n);
+    return 0;
+}
