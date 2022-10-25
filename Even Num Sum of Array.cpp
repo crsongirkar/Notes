f8c4ece8ee9d6e0830a4af25a62a46b16a0cde70
Rohit@08 <73063069+crsongirkar@users.noreply.github.com>
2022-10-25 23:46:34 +0530

    Create Even Num Sum of Array.cpp

---
+++ Even Num Sum of Array.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n=0;
+    int sum=0;
+   int arr[11]={42, 564, 5775, 34, 123, 454, 1, 5, 45, 3556, 23442};
+
+    
+    for(int i=0;i<11;i++)
+    {
+      
+      if(arr[i]%2==0)
+      {
+          
+          cout<<arr[i]<<endl;
+      }
+   
+    
+        
+    }
+}
+// count all even number and print 
+
+// op=  42 5775 34 45 3556
