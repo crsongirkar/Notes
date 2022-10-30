1fd8f85df61333af636adccee5f05ba7c0527a78
Rohit@08 <73063069+crsongirkar@users.noreply.github.com>
2022-10-30 23:54:17 +0530

    Create sample macro.cpp

---
+++ sample macro.cpp
@@ -0,0 +1,27 @@
+//This is a logic code to understand easily what is Macro and how Define a Macro
+
+
+#include<iostream>
+#include<math.h>
+#include<string.h>
+#define Chinmay 10
+#define ans  0
+#define n  20
+#define sum 0
+#define sum n+Chinmay
+#define n1 10
+#define i 10
+//#define for(auto,n1)
+using namespace std;
+
+
+int main()
+{
+  //cin>>n1;
+  for(int j=0;j<=n1;j++)
+  {
+     cout<<j*i<<endl;
+  }
+  
+  
+}
