3ecbcba48327af243001e8e3f88e28a1d52ea04b
Chinmay Songirkar <73063069+crsongirkar@users.noreply.github.com>
2023-04-25 16:34:02 +0530

    Create 1822. Sign of the Product of an Array.cpp

---
+++ 1822. Sign of the Product of an Array.cpp
@@ -0,0 +1,41 @@
+// 1st 
+class Solution {
+public:
+int arraySign(vector<int>& nums) {
+  
+  int count = 1;
+ // int sum = 0;
+ // int x;
+
+  // for(int num=0;num<nums.size();num++)
+  for(auto num : nums)
+  {
+    // sum*=nums[num];
+       
+          if (num == 0) {
+                return 0;
+            }
+            if (num < 0) {
+                count = -1 * count;
+            }
+  
+     
+  }
+     return count;
+    }
+};
+
+//2nd
+
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int product=1;
+        for (int x:nums) {
+            if (x<0) product*=-1;
+            else if (x>0) product*=1;
+            else return 0;
+        }
+        return product;
+    }
+};
